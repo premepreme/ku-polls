1fc81fb1f435ded6d9f56ff35ef7354722db9ab5
preme <ratthicha.p@ku.th>
2022-09-12 00:54:34 +0700

    add unittest

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
