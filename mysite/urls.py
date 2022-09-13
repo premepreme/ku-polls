79141e0c3d673496e2e4bdb99b83c87c77a53c13
preme <ratthicha.p@ku.th>
2022-09-13 20:55:42 +0700

    fix def name

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from polls.views import reversetopoll
+from polls.views import reverse_to_poll
 
 
 urlpatterns = [
-    path('', reversetopoll),
+    path('', reverse_to_poll),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
